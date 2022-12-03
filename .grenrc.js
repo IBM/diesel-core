ed9a7cb43a79bae02e260fa3bd68c2fcf4a89485
Rémi Van Keisbelck <remi@rvkb.com>
2022-12-03 19:16:40 +0100

    cleanup

--- .grenrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-//    "apiUrl": "https://github.com/api/v3",
-    "dataSource": "prs",
-    // "prefix": "v",
-    "includeMessages": "all",
-    "changelogFilename": "CHANGELOG.md",
-    "ignoreIssuesWith": "dependencies"
-}
