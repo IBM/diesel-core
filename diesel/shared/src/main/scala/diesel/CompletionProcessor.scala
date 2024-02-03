2090a7384f496282470496e2157c3ad9a7bc2b5c
Rémi Van Keisbelck <remi@rvkb.com>
2024-02-03 22:31:18 +0100

    add [ and ] to completion delimiters

--- diesel/shared/src/main/scala/diesel/CompletionProcessor.scala
+++ diesel/shared/src/main/scala/diesel/CompletionProcessor.scala
@@ -82,7 +82,7 @@ class CompletionProcessor(
 //    val prefix = findPrefix(text, offset, lookback)
 //    println("prefix:" + prefix)
 
-    val delimiters = ":(){}.,+-".toSet
+    val delimiters = ":(){}.,+-[]".toSet
 
     val c              =
       if (offset >= 1 && offset <= text.length)
