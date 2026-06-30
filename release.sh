c3ea78fa2e8f2e715c9c5590dbb5b7878f0fdb1d
Frank Wagner <agilecoderfrank@gmail.com>
2026-06-30 16:17:02 +0200

    fix release script to check version and publish to npm

--- release.sh
+++ release.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-if [ -z "$TRAVIS_TAG" ]
-then
-   echo "Publishing Scala SNAPSHOT only, ignore yarn."
-   sbt ci-release
-   exit
-fi
+set -ex
 
 # make sure facades / JS has same version as scala jars
-#export VERSION=$(sbt -Dsbt.supershell=false -error "print diesel/version")
-#cd facade && node check-version.js && cd .. && \
-sbt ci-release && \
-sbt samplesBundle/fastOptJS && \
-cd facade && \
-echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc && \
-yarn && \
-cd samples-bundle && \
-echo "dist contents:" && \
-ls dist && \
-yarn publish --access public --verbose && \
-cd  ../ts-facade && \
-yarn build && \
+export VERSION=$(sbt -Dsbt.supershell=false -error "print diesel/version")
+# use facace/bump-version.js, and commit to the release tag 
+( cd facade; ./check-version.js )
+
+sbt ci-release
+sbt samplesBundle/fastOptJS
+
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > .npmrc
+yarn
+cd samples-bundle
+echo "dist contents:"
+ls dist
+yarn publish --access public --verbose
+cd  ../ts-facade
+yarn build
 yarn publish --access public --verbose
